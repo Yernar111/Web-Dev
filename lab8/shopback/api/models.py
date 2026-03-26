b7846ada79edc29a3ab8f541baccaabcd95c1f8a
Yernar111 <ernarsuinullaev1@gmail.com>
2026-03-26 12:38:53 +0500

    fixed a little mistake

--- lab8/shopback/api/models.py
+++ lab8/shopback/api/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = CharField(max_length=20)
+    name = models.CharField(max_length=20)
 
 class Product(models.Model):
-    name = CharField(max_length=20)
-    price = FloatField()
-    description = TextField(max_length=100)
-    count = IntegerField()
-    is_active = BooleanField(default=False)
+    name = models.CharField(max_length=20)
+    price = models.FloatField()
+    description = models.TextField(max_length=100)
+    count = models.IntegerField()
+    is_active = models.BooleanField(default=False)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
