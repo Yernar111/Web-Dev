1cef7eb6d79200e39e964381e7122a9cdfded965
Yernar111 <ernarsuinullaev1@gmail.com>
2026-04-01 23:06:36 +0500

    continued lab8 (lab9)

--- lab8/shopback/api/urls.py
+++ lab8/shopback/api/urls.py
@@ -1,10 +1,21 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
 
-urlpatterns = [
-    path("api/products/", views.products1),
-    path("api/products/<int:id>/", views.product1),
-    path("api/categories/", views.categories1),
-    path("api/categories/<int:id>/", views.categorie1),
-    path("api/categories/<int:id>/products", views.productsByCategory1),
-]
+from rest_framework.routers import DefaultRouter
+from .views import CategoryViewSet, ProductViewSet
+
+router = DefaultRouter()
+
+router.register('categories', CategoryViewSet, basename='category')
+
+router.register('products', ProductViewSet, basename='product')
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path("api/products/", views.products1),
+#     path("api/products/<int:id>/", views.product1),
+#     path("api/categories/", views.categories1),
+#     path("api/categories/<int:id>/", views.categorie1),
+#     path("api/categories/<int:id>/products", views.productsByCategory1),
+# ]
--- lab8/shopback/api/views.py
+++ lab8/shopback/api/views.py
@@ -1,35 +1,59 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+# from django.shortcuts import render
+# from django.http import HttpResponse, JsonResponse
 from .models import Product, Category
 
-def products1(request):
-    products = Product.objects.all()
-    data = list(products.values())
-    return JsonResponse(data, safe=False)
+from rest_framework.viewsets import ModelViewSet
+from .serializers import ProductSerializer, CategorySerializer
 
-def product1(request, id):
-    product = Product.objects.filter(id=id).values().first()
-    if not product:
-        return JsonResponse({"error": "User not found"}, status=404)
-    return JsonResponse(product)
-
-def categories1(request):
-    categories = Category.objects.all()
-    data = list(categories.values())
-    return JsonResponse(data, safe=False)
-
-def categorie1(request, id):
-    categorie = Category.objects.filter(id=id).values().first()
-    if not categorie:
-        return JsonResponse({"error": "User not found"}, status=404)
-    return JsonResponse(categorie)
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
-def productsByCategory1(request, id):
-    products = Product.objects.filter(category=id)
-    data = list(products.values())
-    return JsonResponse(data, safe=False)
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    @action(detail=True, methods=['get'])
+    def products_by_category(self, request, pk=None):
+        category = self.get_object()
+        products = Product.objects.filter(category=category)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+# def products1(request):
+#     products = Product.objects.all()
+#     data = list(products.values())
+#     return JsonResponse(data, safe=False)
+
+# def product1(request, id):
+#     product = Product.objects.filter(id=id).values().first()
+#     if not product:
+#         return JsonResponse({"error": "User not found"}, status=404)
+#     return JsonResponse(product)
+
+# def categories1(request):
+#     categories = Category.objects.all()
+#     data = list(categories.values())
+#     return JsonResponse(data, safe=False)
+
+# def categorie1(request, id):
+#     categorie = Category.objects.filter(id=id).values().first()
+#     if not categorie:
+#         return JsonResponse({"error": "User not found"}, status=404)
+#     return JsonResponse(categorie)
+
+
+# def productsByCategory1(request, id):
+#     products = Product.objects.filter(category=id)
+#     data = list(products.values())
+#     return JsonResponse(data, safe=False)
 
 
 
-# Create your views here.
+# # Create your views here.
